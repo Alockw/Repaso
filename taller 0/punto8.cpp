aaddcc52d949295f2f7e7903fb11439ec56552ef
Alockw <149006455+Alockw@users.noreply.github.com>
2024-02-16 21:51:33 -0500

    .

--- taller 0/punto8.cpp
+++ taller 0/punto8.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 int main() {
-int numero=0;
+int Numero=0;
 cout << "Introduce un numero: ";
-cin >> numero;
-if (numero %  2 ==  0) {
-  cout << numero << " es par" << endl;
+cin >> Numero;
+if (Numero %  2 ==  0) {
+  cout << Numero << " es par" << endl;
 } 
 else {
-  cout << numero << " es impar" << endl;
+  cout << Numero << " es impar" << endl;
 }
 
 return 0;
